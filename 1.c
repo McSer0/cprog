2f4b71dbef51a78a9d0d5ddf0ac7c1f68b17b8fb
Sparow <xofmans@bk.ru>
2024-03-06 19:11:57 +0300

    for in for5x5 init 0 and show

--- 1.c
+++ 1.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  printf("Hallo world!");
+char map[5][5];
 
-  return 0;
+void initMap();
+void showMap();
+
+int main() {
+  initMap();
+  showMap();
+  // printf("%c", map[2][2]);
+}
+
+void initMap() {
+  for (int i = 0; i < 5; i++) {
+    for (int ii = 0; ii < 5; ii++) {
+      map[i][ii] = '0';
+    }
+  }
+}
+
+void showMap() {
+  for (int i = 0; i < 5; i++) {
+    for (int ii = 0; ii < 5; ii++) {
+      printf("%c", map[i][ii]);
+      if (ii == 4) printf("\n");
+    }
+  }
 }
